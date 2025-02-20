3c35ff49a790417664bf6780728ca67c7b3ecb10
dhinuvcn <dineshv17061998@gmail.com>
2025-02-20 03:56:27 -0500

    basic variables

---
+++ Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Type VariableName = Value
+        String Variabletypes = "int, string, boolean, float, double, char";
+        int E = 23;
+        int F = 56;
+        double J = 24.766898d;
+        float s = 45.78f;
+        boolean isjackgoodboy = true;
+        char initial = 'W';
+
+        System.out.println("addition of E&F is:" +(E+F));
+        System.out.println("print double value:" +J);
+        System.out.println("print float value:" +s);
+        System.out.println(isjackgoodboy);
+        System.out.println(initial);
+
+    }
+}
