be590eda27797c960bd0c1b0c40ad75ae593d5df
ponnapati <lourdu9966@gmail.com>
2025-02-20 09:02:18 +0530

    worked on review comments

--- Student.java
+++ Student.java
@@ -1,5 +1,5 @@
 public class Student {
     public static void main(String[] args) {
-        System.out.println("This is my login functionality....");
+        System.out.println("This is my login functionality....this helps users to login to system.");
     }
 }
