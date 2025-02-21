0870e02531103ef5b291e42dd290d526551b4a0e
shivajiparepally <pvshiva2101@gmail.com>
2025-02-21 00:46:11 -0500

    Added Sample class

---
+++ Sample.java
@@ -0,0 +1,16 @@
+public class Sample {
+    public static void main(String[] args) {
+
+        String name="Shiva";
+        int id=2025;
+        char gender='m';
+        double percentage=22.5;
+        boolean b=true;
+        System.out.println("Name : "+name);
+        System.out.println("Id : "+id);
+        System.out.println("Gender : "+gender);
+        System.out.println("Percentage : "+percentage);
+        System.out.println(b);
+
+    }
+}
